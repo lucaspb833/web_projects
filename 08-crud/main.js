abc519965434411e35f89b8f207706eda1ba9129
Lucas da Silva <146649543+lucaspb833@users.noreply.github.com>
2023-10-01 15:17:25 -0300

    Update main.js

--- 08-crud/main.js
+++ 08-crud/main.js
@@ -3,6 +3,7 @@
 const openModal = () => document.getElementById('modal')
     .classList.add('active')
 
+
 const closeModal = () => {
     clearFields()
     document.getElementById('modal').classList.remove('active')
